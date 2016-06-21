ebd054f029b4051bf4faab260421280cc4bd157a
quantumtom <cornyn@gmail.com>
2016-06-21 08:46:52 -0700

    Major overhaul. Convert PHP to NodeJS.

---
+++ src/include/content/generic-footer.php
@@ -0,0 +1,15 @@
+<div class="fake-footer">
+
+	<a href="http://www.verizon.com" rel="external" class="foot-link sticky-left">
+
+		<img src="/src/img/vzw-logo.png" height="25" width="38" alt="Verizon Wireless" />
+
+	</a>
+
+	<a href="http://www.htc.com" rel="external" class="foot-link sticky-right">
+
+		<img src="/src/img/htc-logo.png" height="28" width="90" alt="HTC" />
+
+	</a>
+
+</div>
---
+++ src/include/head.php
@@ -0,0 +1,16 @@
+<title>HTC Rezound</title>
+
+<meta charset="UTF-8" />
+
+<link rel="apple-touch-icon" href="http://www.webkitlab.com/img/htc-icon-114x114.png" />
+
+<link rel="apple-touch-startup-image" href="http://www.webkitlab.com/img/htc-startup.png" />
+
+<meta name="apple-mobile-web-app-capable" content="yes" />
+
+<meta name="viewport" content="height=device-height, width=320, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+
+<link rel="stylesheet" href="/src/css/jquery.mobile-1.0rc2.min.css" />
+
+<link rel="stylesheet" href="/src/css/mobile-extra.css" />
+
